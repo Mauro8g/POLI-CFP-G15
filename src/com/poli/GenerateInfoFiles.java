63b6afee0b16fd9481bf01396ff85139b5ec0da0
Mauro8g <mauro.8g@hotmail.com>
2025-09-04 23:33:02 -0500

    Entrega 1 - Clase GenerateInfoFiles (estructura base y muy teorica)

---
+++ src/com/poli/GenerateInfoFiles.java
@@ -0,0 +1,27 @@
+package com.poli;
+
+/**
+ * Clase GenerateInfoFiles
+ * Entrega 1 - Principios de Programacion
+ *
+ * Objetivo:
+ *  - Disenar la estructura de la clase que generara los archivos de prueba
+ *    (vendedores, productos y ventas).
+ *
+ * Metodos planeados:
+ *  1. createProductsFile(int productsCount)
+ *     -> Generara el archivo products.txt
+ *     -> Formato: IDProducto;NombreProducto;PrecioPorUnidad
+ *
+ *  2. createSalesManInfoFile(int salesmanCount)
+ *     -> Generara el archivo salesmen_info.txt
+ *     -> Formato: TipoDocumento;NumeroDocumento;Nombres;Apellidos
+ *
+ *  3. createSalesMenFile(int randomSalesCount, String name, long id)
+ *     -> Generara un archivo de ventas por vendedor
+ *     -> Cabecera: TipoDocumentoVendedor;NumeroDocumentoVendedor
+ *     -> Cuerpo: IDProducto;Cantidad
+ */
+public class GenerateInfoFiles {
+    // Clase vacia para la entrega teorica (sin implementacion por ahora).
+}
